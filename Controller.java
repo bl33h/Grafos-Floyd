1bbe5f06e84d9bb8077b3315c9bc5a99a0325f01
MelissaPerez09 <per21385@uvg.edu.gt>
2022-05-16 22:41:19 -0600

    update

--- Controller.java
+++ Controller.java
@@ -1,4 +1,5 @@
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Controller {
     /**
@@ -16,11 +17,12 @@ public class Controller {
         Waze waze;
         int numMenu;
         View miVista = new View();
-        miVista.menu();
         String origin;
         String destination;
         int distance;
+        Scanner scan = new Scanner(System.in);
     
+        miVista.menu();
         miVista.welcome(); // Bienvenida al programa
         try{
             boolean flag = true;
@@ -28,6 +30,12 @@ public class Controller {
                 numMenu = miVista.menu();
                 switch(numMenu){
                     case 1: // Ingreso de Ciudad
+                        miVista.output("Ingresar el origen del que comenzara la nueva ruta");
+                        origin = scan.nextLine();
+                        miVista.output("Ingresar el destino del que finalizara la nueva ruta");
+                        destination = scan.nextLine();
+                        miVista.output("Ingresar la distancia que tendra la ruta del destino");
+                        distance = scan.nextInt();
                         waze.newStreet(origin, destination, distance);
                         flag = false;
                         break;
@@ -36,6 +44,11 @@ public class Controller {
                         flag = false;
                         break;
                     case 3: // Interrupcion entre ciudades
+                        miVista.output("Ingresar el origen del que comenzara la interrupcion");
+                        origin = scan.nextLine();
+                        miVista.output("Ingresar el destino del que finalizara la interrupcion");
+                        destination = scan.nextLine();
+                        miVista.output(" ");
                         waze.pauseStreet(origin,destination);
                         flag = false;
                         break;
@@ -44,6 +57,7 @@ public class Controller {
                         flag = false;
                         break;
                     case 5: // Salir
+
                         miVista.end();
                         break;
                 }
