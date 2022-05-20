d1357f047882cd7860b67f8003f6d8b892307ef9
MelissaPerez09 <per21385@uvg.edu.gt>
2022-05-20 11:14:17 -0600

    comments street.java

--- Street.java
+++ Street.java
@@ -5,34 +5,63 @@ public class Street {
     * @author Fabian Juarez, Sara Echeverria, Jose Pablo Kiesling y Melissa Perez
     * FileName: Street
     @version:
-    - Creacion: 13/05/2022
-    - Ultima modificacion: 16/05/2022
-    Clase que establece caracteristicas de los puntos de intercepcion.
+    - Creation: 13/05/2022
+    - Last modification: 20/05/2022
+    Class that establishes the characteristics of the incerpection points.
     */
 
+    //---------------------------PROPERTIES---------------------------
     private String origin;
     private String destination;
     private int distance;
 
+    //---------------------------METHODS------------------------------
+    /****************************************************************
+     * constructor for the class street
+     * @param origin
+     * @param destination
+     * @param distance
+     */
     public Street(String origin, String destination, int distance){
         this.origin = origin;
         this.destination = destination;
         this.distance = distance;
     }
+    //***************************************************************
 
+    /****************************************************************
+     * get origin
+     * @return origin of the street
+     */
     public String getOrigin(){
         return this.origin;
     }
+    //***************************************************************
 
+    /****************************************************************
+     * get destination
+     * @return destination of the street
+     */
     public String getDestination(){
         return this.destination;
     }
+    //***************************************************************
 
+    /****************************************************************
+     * get distance
+     * @return distance of the street
+     */
     public int getDistance(){
         return this.distance;
     }
+    //***************************************************************
 
+    /****************************************************************
+     * set distance
+     * @param distance
+     */
     public void setDistance(int distance){
         this.distance = distance;
     }
+    //***************************************************************
 }
